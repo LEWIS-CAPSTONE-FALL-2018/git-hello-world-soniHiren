48fb8a6bef2a683ed4575415e2f6ce3d4087d1df
soniHiren <hirensoni@lewisu.edu>
2018-09-01 02:09:07 -0500

    Revert "Revert "adding files assignment1.py and readMe""
    This reverts commit 58e31e38b324c03315281f9861a681c86ffd9647.

---
+++ source/repos/assignment1.py/assignment1.py/assignment1.py
@@ -0,0 +1,19 @@
+"""
+Name and Class: Hiren Soni, CPSC-49200-002 Software Systms Capstone Prjct
+Assignment 1
+
+Date: 8/30/2018
+
+Description: This program prints out Hello World to the output screen
+in Python.
+"""
+
+# Defines the main function. 
+def main():
+    # Displays Hello World on the output screen.
+    print("Hello World!")
+
+# Executes the statements found in main() that runs the program.
+if __name__ == "__main__":
+    main()
+
